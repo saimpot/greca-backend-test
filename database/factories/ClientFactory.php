6c1cf70e5fb90d88d5536fb4f28ea0fee7e94fa4
saimpot (George Hatzipavlou) <saimpot@gmail.com>
2022-11-17 14:10:22 +0200

    Create factories

---
+++ database/factories/ClientFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Gender;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Client>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => fake()->firstName,
+            'last_name' => fake()->lastName,
+            'email' => fake()->email,
+            'passport_num' => fake()->randomNumber(9),
+            'gender_id' => fake()->randomElement(Gender::all()),
+        ];
+    }
+}
---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ProductType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    private const CAPACITY = 15;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->text(100),
+            'product_type_id' => fake()->randomElement(ProductType::all()),
+            'description' => fake()->text(255),
+            'capacity' => self::CAPACITY,
+        ];
+    }
+}
